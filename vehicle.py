f96f5921e11a9cbfbdae97c2637d058fd836187a
georgemurage00 <71360279+georgemurage00@users.noreply.github.com>
2023-01-10 23:52:51 +0900

    created a car class
    created a car class

--- vehicle.py
+++ vehicle.py
@@ -10,3 +10,9 @@ class Vehicle:
         self.year = year
         self.rental_rate = rental_rate
         self.available = True
+
+class Car(Vehicle):
+    def __init__(self, make: str, model: str, year: int, rental_rate: int, number_of_doors: int, seats: int):
+        super().__init__(make, model, year, rental_rate)
+        self.number_of_doors = number_of_doors
+        self.seats = seats
